0cd408d06d45a9ac60e680e62f24d3b38b661224
LukasDev <lukas.beneta.dev@gmail.com>
2025-10-30 13:36:23 +0200

    CMS

--- api/callback.js
+++ api/callback.js
@@ -1,55 +1,64 @@
+// /api/callback.js  (ESM)
 export default async function handler(req, res) {
   try {
-    const { OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_REDIRECT_URI } = process.env;
-    if (!OAUTH_CLIENT_ID || !OAUTH_CLIENT_SECRET || !OAUTH_REDIRECT_URI) {
-      res.statusCode = 500;
-      return res.end('Missing OAUTH env vars');
-    }
-
     const url = new URL(req.url, `https://${req.headers.host}`);
-    const code = url.searchParams.get('code');
-    const state = url.searchParams.get('state');
+    const code = url.searchParams.get('code') || '';
+
+    const clientId = process.env.OAUTH_CLIENT_ID;
+    const clientSecret = process.env.OAUTH_CLIENT_SECRET;
+    const redirectUri = process.env.OAUTH_REDIRECT_URI; // https://www.banguklinika.lt/api/callback
 
     if (!code) {
       res.statusCode = 400;
-      return res.end('Missing code');
+      res.setHeader('Content-Type', 'text/html; charset=utf-8');
+      res.end(`<!doctype html><html><body><script>
+        (function(){
+          var msg = 'authorization:github:error:' + JSON.stringify({ message: 'Missing code' });
+          if (window.opener) { window.opener.postMessage(msg, '*'); window.close(); }
+          else { document.body.innerText = 'Missing code'; }
+        })();
+      </script></body></html>`);
+      return;
     }
 
-    const ghResp = await fetch('https://github.com/login/oauth/access_token', {
+    const r = await fetch('https://github.com/login/oauth/access_token', {
       method: 'POST',
-      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        client_id: OAUTH_CLIENT_ID,
-        client_secret: OAUTH_CLIENT_SECRET,
-        code,
-        redirect_uri: OAUTH_REDIRECT_URI,
-      }),
+      headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+      body: JSON.stringify({ client_id: clientId, client_secret: clientSecret, code, redirect_uri: redirectUri }),
     });
+    const data = await r.json();
 
-    const data = await ghResp.json().catch(() => ({}));
     if (!data.access_token) {
-      res.statusCode = 500;
-      return res.end('OAuth failed');
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'text/html; charset=utf-8');
+      res.end(`<!doctype html><html><body><script>
+        (function(){
+          var msg = 'authorization:github:error:' + JSON.stringify({ message: 'OAuth failed' });
+          if (window.opener) { window.opener.postMessage(msg, '*'); window.close(); }
+          else { document.body.innerText = 'OAuth failed'; }
+        })();
+      </script></body></html>`);
+      return;
     }
 
     const html = `<!doctype html><html><body><script>
       (function(){
-        var token=${JSON.stringify(data.access_token)};
-        if (window.opener) {
-          window.opener.postMessage({ token: token, provider: 'github' }, '*');
-          try { localStorage.setItem('decap-cms-auth', JSON.stringify({ token: token })); } catch(e){}
-          window.close();
-        } else {
-          document.body.innerText = 'Token received. You can close this window.';
+        var payload = { token: '${data.access_token}' };
+        var msg = 'authorization:github:success:' + JSON.stringify(payload);
+        function send(){ 
+          if (window.opener) { window.opener.postMessage(msg, '*'); window.close(); }
+          else { document.body.innerText = 'Authorized. You can close this window.'; }
         }
+        send(); setTimeout(send, 250); setTimeout(send, 1000);
       })();
     </script></body></html>`;
 
+    res.setHeader('Cache-Control', 'no-store');
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
     res.end(html);
-  } catch (e) {
-    console.error(e);
+  } catch (err) {
     res.statusCode = 500;
-    res.end('Callback error');
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.end(`<!doctype html><html><body><pre>${(err && err.message) || err}</pre></body></html>`);
   }
 }
